1441d6cad6e94b1fb072388745cdf1de15716518
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-11-19 11:03:00 -0600

    test

---
+++ src/main/java/com/codeup/iknowaspot/IKnowASpotApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.iknowaspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IKnowASpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IKnowASpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/iknowaspot/IKnowASpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.iknowaspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IKnowASpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
