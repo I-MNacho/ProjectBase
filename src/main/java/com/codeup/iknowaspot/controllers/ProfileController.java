559d3563e82699ba6a4e423bb04701e40178bc98
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-11-22 15:08:24 -0600

    fixed errors

--- src/main/java/com/codeup/iknowaspot/controllers/ProfileController.java
+++ src/main/java/com/codeup/iknowaspot/controllers/ProfileController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
-public class ProfileController {
-
-//    @RequestMapping(value="/profile")
-//    public String profile(Model model, Principal principal) {
-//        String name = principal.getName();
-//        model.addAttribute("user", userService.findByUsername(name));
-//        return profile;
-
-    }
-}
+//public class ProfileController {
+//
+////    @RequestMapping(value="/profile")
+////    public String profile(Model model, Principal principal) {
+////        String name = principal.getName();
+////        model.addAttribute("user", userService.findByUsername(name));
+////        return profile;
+//
+//    }
+//}
