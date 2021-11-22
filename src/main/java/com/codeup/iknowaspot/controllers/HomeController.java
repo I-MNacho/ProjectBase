868c73a587aef9a0ff51acf6750da34872bd30da
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-11-21 21:34:34 -0600

    preliminaries

---
+++ src/main/java/com/codeup/iknowaspot/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.codeup.iknowaspot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/splash")
+    @ResponseBody
+    public String homeMessage() {
+        return "This is the landing page!";
+    }
+
+}
+
