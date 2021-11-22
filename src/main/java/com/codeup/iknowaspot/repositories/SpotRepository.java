c79b2f91ccfd79a12c05967b32bae7c5ddeac340
Ignacio Mares <ignacio.mares00@gmail.com>
2021-11-22 11:17:06 -0600

    added spot controller and repo, created delete funciton and create

---
+++ src/main/java/com/codeup/iknowaspot/repositories/SpotRepository.java
@@ -0,0 +1,11 @@
+package com.codeup.iknowaspot.repositories;
+
+import com.codeup.iknowaspot.models.Spot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpotRepository extends JpaRepository<Spot, Long> {
+
+
+
+
+}
