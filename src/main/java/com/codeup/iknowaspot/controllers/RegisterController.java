10edb94a5950aeadad58db751300e7d04017d429
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-11-23 14:07:24 -0600

    fixed splash page link

--- src/main/java/com/codeup/iknowaspot/controllers/RegisterController.java
+++ src/main/java/com/codeup/iknowaspot/controllers/RegisterController.java
@@ -2,11 +2,13 @@ package com.codeup.iknowaspot.controllers;
 
 import com.codeup.iknowaspot.models.User;
 import com.codeup.iknowaspot.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class RegisterController {
 //    public RegisterController(UserRepository userDao, PasswordEncoder passwordEncoder) {
 //        this.userDao = userDao;
@@ -17,7 +19,7 @@ public class RegisterController {
         model.addAttribute("user", new User());
         return "register-form";
     }
-    @PostMapping("/sign-up")
+    @PostMapping("/register")
     public String saveUser(@ModelAttribute User user){
 //        String hash = passwordEncoder.encode(user.getPassword());
 //        user.setPassword(hash);
