07fcf8872cb99e7c7111b73f6fffc90ad997e087
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-11-30 11:44:44 -0600

    still working on api deployment

--- src/main/java/com/codeup/iknowaspot/controllers/HomeController.java
+++ src/main/java/com/codeup/iknowaspot/controllers/HomeController.java
@@ -1,16 +1,24 @@
 package com.codeup.iknowaspot.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 class HomeController {
 
-    @GetMapping("/")
-    public String getIndexPage(Model model) {
-        model.addAt
-        return "index";
+    @Value("${mapbox_key}")
+    private String mapboxApiKey;
+
+    @RequestMapping(path = "/keys.js", produces = "application/javascript")
+    @ResponseBody
+    public String apikey(){
+        System.out.println(mapboxApiKey);
+        return "const MapboxAPIKey = `" + mapboxApiKey + "`";
     }
+
 
     @GetMapping("/aboutus")
     public String getAboutUsPage() {
