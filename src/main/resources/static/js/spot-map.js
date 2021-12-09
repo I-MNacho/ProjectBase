c706efebb89e2e88422cdd219cb1e185cf797e28
Ignacio Mares <ignacio.mares00@gmail.com>
2021-12-09 16:37:45 -0600

    added more on nav bar and search results and one spot page view

---
+++ src/main/resources/static/js/spot-map.js
@@ -0,0 +1,9 @@
+ // th:inline="javascript"
+//     const latitude = [[${latitude}]];
+//     const longitude = [[${longitude}]];
+//     const center = [longitude, latitude];
+//
+//     $(document).ready(() => {
+//     const container = document.getElementById("map");
+//     addMapWithLocation({container, center});
+// })
