222c12de331e1bfcc38bd9696d9a0e4b3efcc927
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-12-12 10:31:03 -0600

    clean up edit profile page

--- src/main/resources/static/js/image-upload.js
+++ src/main/resources/static/js/image-upload.js
@@ -1,13 +1,2 @@
 "use strict";
 const client = filestack.init(FileStackAPIKey);
-function onUploadDone(result) {
-    console.log(result)
-    const url = result.filesUploaded[0].url
-    $('#spot-photo').attr({src: url})
-    $('#spot-photo-input').val(url)
-}
-const picker = client.picker({onUploadDone});
-
-function openFilePicker() {
-    picker.open();
-}
